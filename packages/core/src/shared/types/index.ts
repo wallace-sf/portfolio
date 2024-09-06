4a2572ac17b0a6d44aeaad533f9042d62c23e885
wallace-sf <wallaceedua@gmail.com>
2024-09-06 11:14:36 -0300

    feat: vo long text

--- packages/core/src/shared/types/index.ts
+++ packages/core/src/shared/types/index.ts
@@ -1,6 +1,7 @@
 export * from './DateTime';
 export * from './Description';
 export * from './Id';
+export * from './LongText';
 export * from './Name';
 export * from './ShortText';
 export * from './TechnologyType';
---
+++ packages/core/test/shared/LongText.test.ts
@@ -0,0 +1,47 @@
+import { LongText } from '../../src';
+
+describe('ShortText', () => {
+  describe('when is new', () => {
+    const text = 'Lorem ipsum odor amet, consectetuer adipiscing elit.';
+    const longText = LongText.new(text);
+
+    it('should be valid when text is valid', () => {
+      expect(longText.value).toBe(text);
+      expect(longText.isNew).toBe(false);
+    });
+
+    it('should be invalid when text is invalid', () => {
+      expect(() => LongText.new('')).toThrow(
+        new Error(LongText.ERROR_INVALID_LONG_TEXT),
+      );
+      expect(() => LongText.new()).toThrow(
+        new Error(LongText.ERROR_INVALID_LONG_TEXT),
+      );
+    });
+  });
+
+  describe('when is compared', () => {
+    it('should be valid when two short texts are equal', () => {
+      const text = 'Lorem ipsum odor amet, consectetuer adipiscing elit.';
+
+      const longText1 = LongText.new(text);
+      const longText2 = LongText.new(longText1.value);
+
+      expect(longText1.equals(longText2)).toBe(true);
+      expect(longText1.diff(longText2)).toBe(false);
+    });
+  });
+
+  describe('asserts static method isValid', () => {
+    it('should be valid when param is a text', () => {
+      const text = 'Lorem ipsum odor amet, consectetuer adipiscing elit.';
+
+      expect(LongText.isValid(text)).toBe(true);
+    });
+
+    it('should be invalid when param is empty', () => {
+      expect(LongText.isValid('')).toBe(false);
+      expect(LongText.isValid()).toBe(false);
+    });
+  });
+});
