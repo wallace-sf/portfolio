fdf171dda43273135db1ac065c967fe5afa014be
wallace-sf <wallaceedua@gmail.com>
2024-09-06 10:37:59 -0300

    feat: vo short text

--- packages/core/src/shared/types/index.ts
+++ packages/core/src/shared/types/index.ts
@@ -2,5 +2,6 @@ export * from './DateTime';
 export * from './Description';
 export * from './Id';
 export * from './Name';
+export * from './ShortText';
 export * from './TechnologyType';
 export * from './Url';
