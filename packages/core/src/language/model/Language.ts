fddbe72565aa6a8bfc7b8a2b541f0e51beb26fa8
wallace-sf <wallaceedua@gmail.com>
2024-09-24 15:53:11 -0300

    feat: language locale

--- packages/core/src/language/model/Language.ts
+++ packages/core/src/language/model/Language.ts
@@ -3,15 +3,18 @@ import { Entity, IEntityProps, Name, ShortText } from '../../shared';
 export interface ILanguageProps extends IEntityProps {
   name: string;
   fluency: string;
+  locale: string;
 }
 
 export class Language extends Entity<Language, ILanguageProps> {
   public readonly name: Name;
   public readonly fluency: ShortText;
+  public readonly locale: ShortText;
 
   constructor(props: ILanguageProps) {
     super(props);
     this.name = Name.new(props.name);
     this.fluency = ShortText.new(props.fluency);
+    this.locale = ShortText.new(props.locale);
   }
 }
