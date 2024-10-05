13b159f0d5c6e3fb0f6c8232ab984e6018702048
wallace-sf <wallaceedua@gmail.com>
2024-10-05 16:19:32 -0300

    feat: vo location type (closes #99)

--- packages/core/src/shared/vo/index.ts
+++ packages/core/src/shared/vo/index.ts
@@ -1,6 +1,7 @@
 export * from './DateTime';
 export * from './Fluency';
 export * from './Id';
+export * from './LocationType';
 export * from './Name';
 export * from './SkillType';
 export * from './Text';
