9e0426f3f435ae43a4ff5c777665b907b9553804
wallace-sf <wallaceedua@gmail.com>
2024-10-22 19:32:12 -0300

    feat: product list (#147)

--- apps/web/src/app/[locale]/page.tsx
+++ apps/web/src/app/[locale]/page.tsx
@@ -1,5 +1,106 @@
+import { IProjectProps } from '@repo/core';
+
 import HeroLandingPage from '~assets/images/hero-landing-page.png';
-import { HeroBanner } from '~components/View';
+import { HeroBanner, ProjectList } from '~components/View';
+
+const PROJECTS: IProjectProps[] = [
+  {
+    id: '1',
+    title: 'Fieldlink Enterprise',
+    caption:
+      'Ministro determinou que a Caixa regularize o pagamento à conta certa antes de a PGR analisar a volta da rede social ao ar no Brasil.',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    skills: [],
+  },
+  {
+    id: '2',
+    title: 'Fieldlink Form Builder',
+    caption:
+      'Ministro determinou que a Caixa regularize o pagamento à conta certa antes de a PGR analisar a volta da rede social ao ar no Brasil.',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    skills: [],
+  },
+  {
+    id: '3',
+    title: 'Fieldlink Rotas',
+    caption:
+      'Ministro determinou que a Caixa regularize o pagamento à conta certa antes de a PGR analisar a volta da rede social ao ar no Brasil.',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    skills: [],
+  },
+  {
+    id: '4',
+    title: 'Fieldlink Rotas',
+    caption:
+      'Ministro determinou que a Caixa regularize o pagamento à conta certa antes de a PGR analisar a volta da rede social ao ar no Brasil.',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    skills: [],
+  },
+  {
+    id: '5',
+    title: 'Fieldlink Rotas',
+    caption:
+      'Ministro determinou que a Caixa regularize o pagamento à conta certa antes de a PGR analisar a volta da rede social ao ar no Brasil.',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    skills: [],
+  },
+  {
+    id: '6',
+    title: 'Fieldlink Rotas',
+    caption:
+      'Ministro determinou que a Caixa regularize o pagamento à conta certa antes de a PGR analisar a volta da rede social ao ar no Brasil.',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    skills: [],
+  },
+  {
+    id: '7',
+    title: 'Fieldlink Rotas',
+    caption:
+      'Ministro determinou que a Caixa regularize o pagamento à conta certa antes de a PGR analisar a volta da rede social ao ar no Brasil.',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    skills: [],
+  },
+  {
+    id: '8',
+    title: 'Fieldlink Rotas',
+    caption:
+      'Ministro determinou que a Caixa regularize o pagamento à conta certa antes de a PGR analisar a volta da rede social ao ar no Brasil.',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    skills: [],
+  },
+  {
+    id: '9',
+    title: 'Fieldlink Rotas',
+    caption:
+      'Ministro determinou que a Caixa regularize o pagamento à conta certa antes de a PGR analisar a volta da rede social ao ar no Brasil.',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    skills: [],
+  },
+  {
+    id: '10',
+    title: 'Fieldlink Rotas',
+    caption:
+      'Ministro determinou que a Caixa regularize o pagamento à conta certa antes de a PGR analisar a volta da rede social ao ar no Brasil.',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    skills: [],
+  },
+  {
+    id: '11',
+    title: 'Fieldlink Rotas',
+    caption:
+      'Ministro determinou que a Caixa regularize o pagamento à conta certa antes de a PGR analisar a volta da rede social ao ar no Brasil.',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    skills: [],
+  },
+  {
+    id: '12',
+    title: 'Fieldlink Rotas',
+    caption:
+      'Ministro determinou que a Caixa regularize o pagamento à conta certa antes de a PGR analisar a volta da rede social ao ar no Brasil.',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    skills: [],
+  },
+];
 
 export default function Home() {
   return (
@@ -12,6 +113,8 @@ export default function Home() {
         alt="Professional Picture 1 of Wallace Ferreira"
         imageClassName="object-cover"
       />
+      <h4 className="text-white mt-20 mb-8 mx-[97px]">Projetos</h4>
+      <ProjectList projects={PROJECTS} view="grid" className="pb-20" />
     </>
   );
 }
