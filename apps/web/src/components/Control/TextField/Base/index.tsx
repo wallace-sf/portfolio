0231526e5c81f9f4869c1781fe32f4f9c127206b
wallace-sf <wallaceedua@gmail.com>
2024-10-27 21:40:30 -0300

    feat(components): label (#162)

--- apps/web/src/components/Control/TextField/Base/index.tsx
+++ apps/web/src/components/Control/TextField/Base/index.tsx
@@ -38,7 +38,7 @@ const Component: ForwardRefRenderFunction<
           '!border-error': error && errorBorder && touched && !unstyled,
           '!border-accent':
             error == null && errorBorder && touched && !unstyled,
-          'border-2 border-dark-500 bg-dark-300 h-12 rounded-xl p-3 w-full text-sm font-medium outline-none placeholder:font-normal focus:!border-primary active:!border-primary placeholder:text-gray-300 disabled:bg-whiter disabled:cursor-default dark:text-white':
+          'border-2 border-dark-500 bg-dark-300 h-12 rounded-xl p-3 w-full text-sm font-medium outline-none placeholder:font-normal focus:!border-primary active:!border-primary placeholder:text-dark-500 disabled:bg-whiter disabled:cursor-default dark:text-white':
             !unstyled,
         },
         props.className,
