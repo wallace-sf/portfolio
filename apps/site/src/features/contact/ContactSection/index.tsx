791abf43abfe615a35119dd5e3fe571fc49a9ff2
wallace-sf <wallaceedua@gmail.com>
2026-06-07 18:37:10 -0300

    style: remove border bottom from header when is XL bp

--- apps/site/src/features/contact/ContactSection/index.tsx
+++ apps/site/src/features/contact/ContactSection/index.tsx
@@ -5,7 +5,7 @@ import { ContactInfo } from '~features/contact/ContactInfo';
 
 export function ContactSection() {
   return (
-    <section className="bg-surface-overlay xl:border-2 xl:border-border-subtle py-10 xl:-mx-[161px] xl:px-[161px]">
+    <section className="bg-surface-overlay xl:border-2 xl:border-b-0 xl:border-border-subtle py-10 xl:-mx-[161px] xl:px-[161px]">
       <div className="flex flex-col gap-y-6 xl:flex-row xl:gap-x-16">
         <div className="xl:w-[560px] shrink-0">
           <ContactForm />
